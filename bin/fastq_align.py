2746384236cbc0bcff6a7c5784f68e2d1bc54ee7
timothyjamesbecker <timothyjamesbecker@gmail.com>
2020-03-25 09:32:31 -0400

    fastq_align gains some restart and multi-fastq sample folder functionality for greater automation

--- bin/fastq_align.py
+++ bin/fastq_align.py
@@ -18,7 +18,7 @@ parser.add_argument('-o','--out',type=str,help='reference fasta input file\t[fas
 parser.add_argument('--tools',type=str,help='minimap2,samtools,sambamba tools path\t[None]')
 parser.add_argument('--threads',type=int,help='reference fasta input file\t[4]')
 parser.add_argument('--platform',type=str,help='illumina,pacbio\t[illumina]')
-parser.add_argument('--fast',type=bool,help='use more cpu and memory intensive alignment\t[False]')
+parser.add_argument('--fast',action='store_true',help='use more cpu and memory intensive alignment\t[False]')
 parser.add_argument('--clean',type=bool,help='delete fastq files as you proceed to save disk\t[False]')
 args = parser.parse_args()
 
